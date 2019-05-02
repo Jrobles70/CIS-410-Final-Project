9fcd2c6cee86e91f9800a84dfea88aba58e5d88a
Justin <j.70.robles@gmail.com>
2019-05-01 19:16:17 -0700

    Add files

---
+++ Assets/LowpolyStreetPack/Scripts/MoveRoad.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRoad : MonoBehaviour
+{
+    public int speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the object forward along its z axis 1 unit/second.
+        transform.Translate(speed * Vector3.back * Time.deltaTime);
+    }
+}
