dee8be3ced8a6096ac7504be679b32241ae34662
Justin <j.70.robles@gmail.com>
2019-05-03 18:23:21 -0700

    POC changes

---
+++ Assets/Scripts/DestroyByBounds.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByBounds : MonoBehaviour
+{
+	void OnTriggerExit (Collider other) {
+		Destroy (other.gameObject);
+		// Debug.Log ("Destroyed " + other.name);
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+	public GameObject [] cars;
+	public Transform transform;
+	public float spawn_wait;
+	public float z_pos;
+	public float x_min, x_max;
+	public float speed;
+
+	private IEnumerator spawn () {
+
+		while (true) {
+
+			GameObject car;
+			car = cars [Random.Range (0, cars.Length)];
+			Vector3 position = new Vector3 (Random.Range (x_min, x_max), 0.0f, z_pos);
+			Instantiate (car, position, transform.rotation);
+			yield return new WaitForSeconds (spawn_wait);
+		}	
+	}
+
+	void Start () {
+		Debug.Log (gameObject.tag + " started");
+		StartCoroutine( spawn ());
+	}
+
+}
